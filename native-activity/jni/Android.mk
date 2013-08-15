82bfa6c4891285438be691251330bcece7a0f0c8
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-08-15 12:04:17 +0900

    Extract Ajhc RTS when compile time.

--- native-activity/jni/Android.mk
+++ native-activity/jni/Android.mk
@@ -17,8 +17,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
-LOCAL_SRC_FILES := main.c
+LOCAL_SRC_FILES := main.c dummy4jhc.c \
+		   ../hs_build/rts/rts_support.c ../hs_build/rts/jhc_rts.c \
+		   ../hs_build/lib/lib_cbits.c ../hs_build/rts/gc_jgc.c \
+		   ../hs_build/rts/stableptr.c ../hs_build/rts/conc.c ../hs_build/hs_main.c
+LOCAL_C_INCLUDES := hs_build/cbits hs_build
+LOCAL_CFLAGS    := -std=gnu99 -D_GNU_SOURCE -falign-functions=4 -ffast-math -fno-strict-aliasing \
+		   -DNDEBUG -D_JHC_GC=_JHC_GC_JGC -D_JHC_CONC=_JHC_CONC_PTHREAD -D_JHC_USE_OWN_STDIO
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_ARM_MODE  := arm
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 include $(BUILD_SHARED_LIBRARY)
