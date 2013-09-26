e6cac399e01cef089da6ec473224ab7155a3c690
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-09-26 10:44:57 +0900

    Start to snatch engine_init_display function. (cont.)

--- native-activity/hs_src/AndroidNdk.hs
+++ native-activity/hs_src/AndroidNdk.hs
@@ -237,8 +237,19 @@ foreign export ccall "engineTermDisplay" engineTermDisplay :: Ptr AndroidEngine 
 
 
 -- OpenGL ES
+type GLenum = Word32
+type GLbitfield = Word32
+foreign import ccall "c_extern.h glHint" c_glHint :: GLenum -> GLenum -> IO ()
+foreign import ccall "c_extern.h glEnable" c_glEnable:: GLenum -> IO ()
+foreign import ccall "c_extern.h glShadeModel" c_glShadeModel :: GLenum -> IO ()
+foreign import ccall "c_extern.h glDisable" c_glDisable :: GLenum -> IO ()
 foreign import ccall "c_extern.h glClearColor" c_glClearColor :: Float -> Float -> Float -> Float -> IO ()
-foreign import ccall "c_extern.h glClear" c_glClear :: Word32 -> IO ()
+foreign import ccall "c_extern.h glClear" c_glClear :: GLbitfield -> IO ()
+foreign import primitive "const.GL_PERSPECTIVE_CORRECTION_HINT" c_GL_PERSPECTIVE_CORRECTION_HINT :: GLenum
+foreign import primitive "const.GL_FASTEST" c_GL_FASTEST :: GLenum
+foreign import primitive "const.GL_CULL_FACE" c_GL_CULL_FACE :: GLenum
+foreign import primitive "const.GL_SMOOTH" c_GL_SMOOTH :: GLenum
+foreign import primitive "const.GL_DEPTH_TEST" c_GL_DEPTH_TEST :: GLenum
 foreign import primitive "const.GL_COLOR_BUFFER_BIT" c_GL_COLOR_BUFFER_BIT :: Word32
 
 engineDrawFrame :: Ptr AndroidEngine -> IO ()
@@ -259,3 +270,16 @@ engineDrawFrame eng = peek eng >>= go
             void $ c_eglSwapBuffers disp surf
 
 foreign export ccall "engineDrawFrame" engineDrawFrame :: Ptr AndroidEngine -> IO ()
+
+
+engineInitDisplay :: Ptr AndroidEngine -> IO Int
+engineInitDisplay eng = peek eng >>= go >>= poke eng >> return 0
+  where go :: AndroidEngine -> IO AndroidEngine
+        go enghs = do
+          c_glHint       c_GL_PERSPECTIVE_CORRECTION_HINT c_GL_FASTEST
+          c_glEnable     c_GL_CULL_FACE
+          c_glShadeModel c_GL_SMOOTH
+          c_glDisable    c_GL_DEPTH_TEST
+          return enghs
+
+foreign export ccall "engineInitDisplay" engineInitDisplay :: Ptr AndroidEngine -> IO Int
